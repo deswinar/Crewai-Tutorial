046cbe12a1ddffe39cdc99d0edc5c4eaee6f0177
deswinar <deswinauliyaarabbani@gmail.com>
2025-01-29 21:25:30 +0700

    update requirements

--- src/latest_ai_development/app.py
+++ src/latest_ai_development/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import os
 import json
 from latest_ai_development.main import run_crew  # Import the new function
+
+os.environ["MODEL"] = st.secrets["OPENAI_API_KEY"]
+os.environ["GEMINI_API_KEY"] = st.secrets["GROQ_API_KEY"]
+os.environ["SERPER_API_KEY"] = st.secrets["SERPER_API_KEY"]
 
 st.title("CrewAI Runner")
 
